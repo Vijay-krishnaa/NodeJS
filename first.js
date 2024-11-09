da20598f05a40158b175225115f40882f7252095
Vijay Krishna <vkk7739@gmail.com>
2024-11-09 14:56:17 +0530

    LearningNode 1st prg

--- first.js
+++ first.js
@@ -1,1 +1,10 @@
-console.log("hi");
+// console.log("hi");
+const fs = require("fs");
+fs.writeFile("output.txt",'writing File',(err)=>{
+  if(err)console.log("error found");
+    
+    
+  
+  else console.log('file written successfully');
+  
+});
