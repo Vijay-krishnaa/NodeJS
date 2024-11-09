2635ad362387db20c9412d4e866b0df1534e91f0
Vijay Krishna <vkk7739@gmail.com>
2024-11-10 00:18:05 +0530

    Sending response

--- App.js
+++ App.js
@@ -1,6 +1,30 @@
 const http = require('http')
 const server = http.createServer((req , res)=>{
-  console.log(req);
+  // console.log(req.url,req.method,req.headers);
+  // process.exit();
+  if(req.url==='/home'){
+    res.setHeader('Contenet-Type' , 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Node Js</title></head>');
+  res.write('<body>welcome To Home</body>');
+  res.write('</html>');
+  return res.end();
+  }
+  else if(req.url==='/products') 
+    {res.setHeader('Contenet-Type' , 'text/html');
+      res.write('<html>');
+      res.write('<head><title>Node Js</title></head>');
+      res.write('<body>Click here to view products</body>');
+      res.write('</html>');
+      return res.end()
+  }
+  {
+    res.setHeader('Contenet-Type' , 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Node Js</title></head>');
+    res.write('<body>hello</body>');
+    res.write('</html>');
+  }
   
 }
 )
