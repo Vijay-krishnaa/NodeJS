6897b7fd18f9cf8732b62d0db80f81edb3782eca
Vijay Krishna <vkk7739@gmail.com>
2024-11-12 20:50:56 +0530

    Express.js

--- practice/App.js
+++ practice/App.js
@@ -1,13 +1,14 @@
-
-
+const requesthandler = require('../practice/form')
 const http = require('http')
+const express = require('express')
+const app  =express();
+app.use((req,res,next)=>{
+  res.send('<h1>Welcome to Express</h1>')
+  // next()
 
-const server = http.createServer((req,res
-  
-)=>{
-  console.log(req);
-  
 })
+
+const server = http.createServer(app)
 const port = 3002
 server.listen(port,()=>{
   console.log(`Server is running on ${port}`);
