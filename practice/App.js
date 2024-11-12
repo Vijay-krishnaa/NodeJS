2176bd4e22774c7eb36d63631c32e4c0fbba5924
Vijay Krishna <vkk7739@gmail.com>
2024-11-12 21:51:28 +0530

    learning - express routes

--- practice/App.js
+++ practice/App.js
@@ -1,16 +1,15 @@
 const requesthandler = require('../practice/form')
-const http = require('http')
 const express = require('express')
 const app  =express();
-app.use((req,res,next)=>{
-  res.send('<h1>Welcome to Express</h1>')
-  // next()
-
+app.use("/",(req,res,next)=>{
+res.send('<h1>WELCOME TO HOME</h1>')
+next()
 })
+app.use("/form",(req,res,next)=>{
 
-const server = http.createServer(app)
-const port = 3002
-server.listen(port,()=>{
-  console.log(`Server is running on ${port}`);
   
+})
+const port = 3002
+app.listen(port,()=>{
+  console.log(`Server is running on ${port}`); 
 })
