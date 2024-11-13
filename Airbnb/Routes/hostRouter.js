358f9ef49943ea1122efa97b1aa612657a79e477
Vijay Krishna <vkk7739@gmail.com>
2024-11-14 01:27:34 +0530

    file-helper

--- Airbnb/Routes/hostRouter.js
+++ Airbnb/Routes/hostRouter.js
@@ -2,8 +2,9 @@ const path = require("path");
 const express = require("express");
 
 const hostRouter = express.Router();
+const rootDir = require("../utils/pathutlis");
 hostRouter.get("/host-home", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "login.html"));
+  res.sendFile(path.join(rootDir, "views", "login.html"));
 });
 
 hostRouter.post("/host-login", (req, res, next) => {
--- Airbnb/Routes/userrRouter.js
+++ Airbnb/Routes/userrRouter.js
@@ -3,7 +3,8 @@ const path = require("path");
 const express = require("express");
 
 const userRouter = express.Router();
+const rootDir = require("../utils/pathutlis");
 userRouter.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "views", "home.html"));
+  res.sendFile(path.join(rootDir, "views", "home.html"));
 });
 module.exports = userRouter;
