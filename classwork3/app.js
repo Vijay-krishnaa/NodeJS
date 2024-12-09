f46253df414a3bd0a9f4446e8ea7b4749733ec7c
Vijay Krishna <vkk7739@gmail.com>
2024-12-09 19:57:37 +0530

    using cors library

--- classwork3/app.js
+++ classwork3/app.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 // const upload = multer({ dest: "./assets" });
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./assets");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, "vicky" + path.extname(file.originalname));
   },
 });
 const upload = multer({ storage: storage });
 app.get("/", (req, res) => {
-  res.send("server is running");
+  res.json("server is running");
 });
 app.post("/myfile", upload.none(), (req, res) => {
   res.send(req.body);
@@ -21,6 +24,7 @@ app.post("/myfile", upload.none(), (req, res) => {
 app.post("/image", upload.single("image"), (req, res) => {
   res.send("image uploded successfully");
 });
+
 const port = 3002;
 app.listen(port, () => {
   console.log(`server is running at http://localhost:${port}`);
