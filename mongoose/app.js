a545cbc4631183a9b30ec99f99b85652b1e64579
Vijay Krishna <vkk7739@gmail.com>
2024-12-19 18:30:25 +0530

    mongodb

--- mongoose/app.js
+++ mongoose/app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
